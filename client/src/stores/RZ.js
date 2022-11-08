5cdac11ae9a74263fd877cef0e3961d309fef4d0
Rahmat <rahmathikaru03@gmail.com>
2022-11-08 14:37:52 +0700

    feature-login

--- client/src/stores/RZ.js
+++ client/src/stores/RZ.js
@@ -1,7 +1,40 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
 export const useRZStore = defineStore("RZ", {
-  state: () => ({}),
+  state: () => ({
+    checkLogin: false,
+  }),
   getters: {},
-  actions: {},
+  actions: {
+    async logout() {
+      localStorage.clear();
+      this.checkLogin = false;
+      this.router.push("/login");
+    },
+    async loginToSite(email, password) {
+      try {
+        let { data } = await axios.post(`${baseUrl}/pub/login`, {
+          email,
+          password,
+        });
+        localStorage.setItem(`access_token`, data.access_token);
+        localStorage.setItem(`username`, data.username);
+
+        this.checkLogin = true;
+        this.router.push("/");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async register() {
+      try {
+        const { data } = await axios.post(`${baseUrl}/register`, this.user);
+        this.checkLogin = false;
+        this.router.push("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 });
