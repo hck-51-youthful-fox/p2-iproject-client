dd04de7be2df93835c34851beb9594deb0bfb98a
riemogerz <rizqanhukum@gmail.com>
2022-11-08 21:51:39 +0700

    Membuat tampilan login dan register

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import { createPinia } from 'pinia'
+const pinia = createPinia()
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
+pinia.use(({ store }) => {
+	store.route = markRaw(router)
+})
+
 app.use(router)
 
 app.mount('#app')
