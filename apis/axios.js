ba179922782abda91872c8a05f11c42e8d72a9e2
leonardhw <leonardharyow@gmail.com>
2022-11-10 12:07:08 +0700

    asd

--- apis/axios.js
+++ apis/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://over-game-pass.herokuapp.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://over-game-pass.herokuapp.com",
 });
 
 export default instance;
