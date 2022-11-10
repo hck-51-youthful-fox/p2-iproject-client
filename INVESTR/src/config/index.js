f6e5542b0ee28839dbf8b4b4e4a19fe589250b43
Rizqi irfan <r.irfannawwaf@gmail.com>
2022-11-10 11:43:37 +0700

    change baseUrl

--- INVESTR/src/config/index.js
+++ INVESTR/src/config/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://fast-brushlands-90897.herokuapp.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://investr.herokuapp.com/",
+  // baseURL: "http://localhost:3000",
 });
 
 export default instance;
