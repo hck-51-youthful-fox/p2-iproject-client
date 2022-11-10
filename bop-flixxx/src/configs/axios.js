3768f35507b0829cda7f940529a702b4ecf41a68
ivanarifin <ivan.rarifin@gmail.com>
2022-11-10 07:47:09 +0700

    deploy

--- bop-flixxx/src/configs/axios.js
+++ bop-flixxx/src/configs/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://jobystreet.herokuapp.com/pub",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://bopflix.herokuapp.com",
+  // baseURL: "http://localhost:3000",
 });
 
 export default instance;
