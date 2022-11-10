d118d098b1b7a2dcbdb9ed3c4eb320150a057944
Rahmat <rahmathikaru03@gmail.com>
2022-11-10 12:15:26 +0700

    setip deploy firebased

--- client/src/stores/RZ.js
+++ client/src/stores/RZ.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-const baseUrl = `http://localhost:3000`;
+const baseUrl = `https://secure-woodland-38516.herokuapp.com`;
 
 export const useRZStore = defineStore("RZ", {
   state: () => ({
