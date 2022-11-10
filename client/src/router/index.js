afdae6c8a669c006b28ed8ccba055cabeaa84657
owldaylong <anastasiameicy@gmail.com>
2022-11-10 09:20:13 +0700

    added login page & google login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "../views/LandingPage.vue";
 import MenuPage from "../views/MenuPage.vue";
 import CartModal from "../views/CartModal.vue";
+import LoginPage from "../views/LoginPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
       path: "/",
       name: "landing-page",
       component: LandingPage,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginPage,
     },
     {
       path: "/beverages",
