10d838e46afd2c9834bd36e18a913793a484884a
Yossy Adirta <yossyadirta@gmail.com>
2022-11-14 01:33:34 +0700

    Change baseUrl to localhost

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:3000",
-  baseURL: "https://makans-indonesia.herokuapp.com",
+  baseURL: "http://localhost:3000",
+  // baseURL: "https://makans-indonesia.herokuapp.com",
 });
 
 export default instance;
